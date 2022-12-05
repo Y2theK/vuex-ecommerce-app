84e70bf37a6fb1264440637e99e9b65f25ebaa1b
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-12-05 23:05:04 +0630

    template setup

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Boutique | Ecommerce bootstrap template</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="all,follow" />
+
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="../../template/img/favicon.png" />
+  </head>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <!DOCTYPE html>
+  <html>
+    <Header />
+    <body>
+      <div class="page-holder">
+        <!-- navbar-->
+        <Navbar />
+
+        <div class="container">
+          <!-- HERO SECTION-->
+          <HeroSection />
+
+          <!-- CATEGORIES SECTION-->
+          <CategoriesList />
+
+          <!-- TRENDING PRODUCTS-->
+          <ProductsList />
+
+          <!-- SERVICES-->
+          <Services />
+
+          <!-- NEWSLETTER-->
+          <NewsLetter />
+        </div>
+
+        <Footer />
+
+        <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+          crossorigin="anonymous"
+        />
+      </div>
+    </body>
+  </html>
+</template>
+
+<script src="../assets/myjs.js"></script>
+<script>
+import ProductsList from "./ProductsList.vue";
+import CategoriesList from "./CategoriesList.vue";
+import Navbar from "./Navbar.vue";
+import Services from "./Services.vue";
+import NewsLetter from "./NewsLetter.vue";
+import Footer from "./Footer.vue";
+import HeroSection from "./HeroSection.vue";
+import Header from "./Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProductsList,
+    CategoriesList,
+    Navbar,
+    Services,
+    NewsLetter,
+    Footer,
+    HeroSection,
+    Header,
+  },
+};
+</script>
+<style>
+@import url("../assets/mycss.css");
+</style>
