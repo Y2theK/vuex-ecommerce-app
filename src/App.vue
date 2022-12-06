f5e9596e9f9d05b40b119f559790fc504374fd6c
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-12-06 21:33:28 +0630

    router-link and router-view setup for Home and ProductDetail

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
 export default {
   name: "App",
-  components: {
-    Home,
-  },
-  // mounted() {
-  //   console.log(this.$store.state.Product.products);
-  // },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import VueRouter from "vue-router";
 import Vue from "vue";
 import ProductDetail from "../components/ProductDetail.vue";
+import Home from "../components/Home.vue";
 
 Vue.use(VueRouter);
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
   {
     path: "/products/:id",
     name: "product-detail",
