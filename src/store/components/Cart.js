583b9fa81947ecc5d003034797d15af0eed6006a
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-12-12 23:08:16 +0630

    prepare cart store as vuex module

--- src/store/components/Cart.js
+++ src/store/components/Cart.js
@@ -12,11 +12,43 @@ export default {
     setCarts(state, carts) {
       state.carts = carts;
     },
+    addNewCart(state, cart) {
+      state.carts.unshift(cart);
+    },
+    updateCart(state, cart) {
+      state.carts.forEach((c) => {
+        if (c.id === cart.id) {
+          c = cart;
+        }
+      });
+    },
+    deleteCart(state, removeId) {
+      state.carts = state.carts.filter((cart) => {
+        return cart.id !== removeId;
+      });
+    },
   },
   actions: {
     async getUserCarts({ commit }) {
       let res = await axios.get("https://fakestoreapi.com/carts/user/2");
       commit("setCarts", res.data);
+    },
+    async addNewCart({ commit }, newCart) {
+      let res = await axios.post("https://fakestoreapi.com/carts", newCart);
+      console.log(res.data);
+      commit("addNewCart", res.data);
+    },
+    async updateCart({ commit }, cart) {
+      let res = await axios.put("https://fakestoreapi.com/carts/" + cart.id);
+      console.log(res.data);
+      commit("update", res.data);
+    },
+    async deleteCart({ commit }, removeId) {
+      let res = await axios.delete(
+        "https://fakestoreapi.com/carts/" + removeId
+      );
+      console.log(res.data);
+      commit("deleteCart", removeId);
     },
   },
 };
