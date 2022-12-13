89a920c4d8b4236201d7d65e8e31369b2ad05779
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-12-13 23:33:13 +0630

    render cart-products in cart table

--- src/components/CartTable.vue
+++ src/components/CartTable.vue
@@ -20,27 +20,34 @@
           </th>
         </tr>
       </thead>
-      <tbody class="border-0">
+      <tbody class="border-0" v-for="product in Products" :key="product.id">
         <tr>
           <th class="ps-0 py-3 border-light" scope="row">
             <div class="d-flex align-items-center">
-              <a class="reset-anchor d-block animsition-link" href="detail.html"
-                ><img
-                  src="../../template/img/product-detail-3.jpg"
-                  alt="..."
-                  width="70"
-              /></a>
+              <router-link
+                class="reset-anchor d-block animsition-link"
+                :to="{ name: 'product-detail', params: { id: product.id } }"
+                ><img :src="product.image" alt="..." width="50"
+              /></router-link>
               <div class="ms-3">
-                <strong class="h6"
-                  ><a class="reset-anchor animsition-link" href="detail.html"
-                    >Red digital smartwatch</a
-                  ></strong
-                >
+                <small class="">
+                  <router-link
+                    class="reset-anchor animsition-link"
+                    :to="{ name: 'product-detail', params: { id: product.id } }"
+                  >
+                    <span v-if="product.title.length < 30">
+                      {{ product.title }}
+                    </span>
+                    <span v-else>
+                      {{ product.title.substring(0, 30) + "..." }}
+                    </span>
+                  </router-link>
+                </small>
               </div>
             </div>
           </th>
           <td class="p-3 align-middle border-light">
-            <p class="mb-0 small">$250</p>
+            <p class="mb-0 small">${{ product.price }}</p>
           </td>
           <td class="p-3 align-middle border-light">
             <div
@@ -74,64 +81,6 @@
             <p class="mb-0 small">$250</p>
           </td>
           <td class="p-3 align-middle border-light">
-            <a class="reset-anchor" href="#!"
-              ><i class="fas fa-trash-alt small text-muted"></i
-            ></a>
-          </td>
-        </tr>
-        <tr>
-          <th class="ps-0 py-3 border-0" scope="row">
-            <div class="d-flex align-items-center">
-              <a class="reset-anchor d-block animsition-link" href="detail.html"
-                ><img
-                  src="../../template/img/product-detail-2.jpg"
-                  alt="..."
-                  width="70"
-              /></a>
-              <div class="ms-3">
-                <strong class="h6"
-                  ><a class="reset-anchor animsition-link" href="detail.html"
-                    >Apple watch</a
-                  ></strong
-                >
-              </div>
-            </div>
-          </th>
-          <td class="p-3 align-middle border-0">
-            <p class="mb-0 small">$250</p>
-          </td>
-          <td class="p-3 align-middle border-0">
-            <div
-              class="
-                border
-                d-flex
-                align-items-center
-                justify-content-between
-                px-3
-              "
-            >
-              <span class="small text-uppercase text-gray headings-font-family"
-                >Quantity</span
-              >
-              <div class="quantity">
-                <button class="dec-btn p-0">
-                  <i class="fas fa-caret-left"></i>
-                </button>
-                <input
-                  class="form-control form-control-sm border-0 shadow-0 p-0"
-                  type="text"
-                  value="1"
-                />
-                <button class="inc-btn p-0">
-                  <i class="fas fa-caret-right"></i>
-                </button>
-              </div>
-            </div>
-          </td>
-          <td class="p-3 align-middle border-0">
-            <p class="mb-0 small">$250</p>
-          </td>
-          <td class="p-3 align-middle border-0">
             <a class="reset-anchor" href="#!"
               ><i class="fas fa-trash-alt small text-muted"></i
             ></a>
@@ -152,10 +101,6 @@ export default {
   methods: mapActions(["getUserCart"]),
   created() {
     this.getUserCart();
-  },
-  mounted() {
-    console.log(this.cart.userId);
-    console.log(this.Products);
   },
 };
 </script>
